416ac2044cf4e3af9463894a51a3c9e0abe2b232
unknown <morenobenitezvictor@gmail.com>
2024-02-13 13:30:24 +0100

    Añado ejercicios

--- T2P2/jscode/ejercicio2.js
+++ T2P2/jscode/ejercicio2.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    
+    let boton = document.querySelectorAll("body input")[0];
 
-
+    boton.addEventListener("click", function() {
+        
+        fetch("https://www.jaimeweb.es/medac/datos.json")
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach((usuario) => {
+                console.log(usuario);
+            });
+            for(let user of data) {
+                let claves = Object.values(user);
+                for(let propiedades of claves) {
+                    console.log(propiedades);
+                }
+            }
+        });
+    });
 });
