90fba8b8405934e146b0a64506a8d291a75eabac
unknown <morenobenitezvictor@gmail.com>
2024-02-13 12:57:00 +0100

    Añado ejercicio 1 practica

---
+++ T2P2/jscode/ejercicio1.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    let boton1 = document.querySelectorAll("section input")[0];
+    let boton2 = document.querySelectorAll("section input")[1];
+
+    boton1.addEventListener("click", function() {
+        fetch("https://randomuser.me/api")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        });
+    });
+
+    boton2.addEventListener("click", function() {
+        fetch("https://randomuser.me/api") 
+        .then((response) => response.json())
+        .then((data) => {
+
+            let nombre = data.results[0].name.first;
+            let apellido = data.results[0].name.last;
+            let edad = data.results[0].dob.age;
+            let imagen = document.createElement("img");
+            imagen.setAttribute("src", data.results[0].picture.medium)
+            imagen.setAttribute("alt", nombre);
+
+            let nodoInfo = document.createElement("p");
+            let texto = document.createTextNode(nombre + " " + apellido + ": " + edad);
+            nodoInfo.appendChild(texto);
+
+            let article = document.createElement("article");
+            article.setAttribute("class", "usuario");
+            article.appendChild(nodoInfo);
+            article.appendChild(imagen);
+
+            let div = document.querySelector("section div");
+            div.appendChild(article);
+        });
+    });
+});
